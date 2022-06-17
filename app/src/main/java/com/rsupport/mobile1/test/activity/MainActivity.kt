03e926ab4e4e3c0f50198e1f8eb00295b89cc700
dev-cardone <dev-cardone@gmail.com>
2022-06-17 16:09:00 +0900

    feat(search): ActionBar에 Search View 추가

--- app/src/main/java/com/rsupport/mobile1/test/activity/MainActivity.kt
+++ app/src/main/java/com/rsupport/mobile1/test/activity/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.rsupport.mobile1.test.activity
 
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.widget.SearchView
 import androidx.appcompat.app.AppCompatActivity
 import com.rsupport.mobile1.test.R
 import dagger.hilt.android.AndroidEntryPoint
@@ -11,5 +14,29 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.search_menu, menu)
+
+        val searchView = menu?.findItem(R.id.search)?.actionView as SearchView
+        searchView.setOnQueryTextListener(OnQueryTextListenerImpl())
+        searchView.queryHint = "이미지 검색어 입력"
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    private inner class OnQueryTextListenerImpl : SearchView.OnQueryTextListener {
+        override fun onQueryTextSubmit(p0: String?): Boolean {
+            // TODO: 해당 검색어로 Getty 이미지 크롤링 요청
+            Log.d(TAG, "onQueryTextSubmit: $p0")
+            return true
+        }
+
+        override fun onQueryTextChange(p0: String?): Boolean = true
+    }
+
+    companion object {
+        private const val TAG = "MainActivity"
     }
 }
