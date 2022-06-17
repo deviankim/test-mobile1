d88b62e7cfe55183eb7caf101e1aba876b97bae6
dev-cardone <dev-cardone@gmail.com>
2022-06-17 14:13:26 +0900

    fix(image-load): 이미지 로드 비동기 처리 관련 버그 수정
    Problem.
    이미지 다운로드가 끝나기 전에 ViewHolder가 재사용 되어 새로운 url binding 요청이 들어온 경우, 간헐적으로 먼저 요청한 bind 요청이 나중에 완료되어 이미지가 정상적으로 표출되지 않음.
    
    즉, 마지막으로 bind 요청된 이미지가 보여야 하는데 이전에 bind 요청된 이미지가 늦게 완료되어 원치않는 이미지가 표출되는 현상임.
    
    Solution.
    새로운 bind 요청이 들어온 경우 직전에 요청한 이미지 다운로드 작업을 취소하도록 처리.

--- app/src/main/java/com/rsupport/mobile1/test/activity/ui/GettyImageViewHolder.kt
+++ app/src/main/java/com/rsupport/mobile1/test/activity/ui/GettyImageViewHolder.kt
@@ -3,15 +3,17 @@ package com.rsupport.mobile1.test.activity.ui
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.rsupport.mobile1.test.activity.utils.RemoteImageLoader.loadRemoteImage
+import com.rsupport.mobile1.test.activity.utils.RemoteImageLoader
 import com.rsupport.mobile1.test.databinding.ListItemGettyImageBinding
 
 class GettyImageViewHolder(
     private val binding: ListItemGettyImageBinding
 ) : RecyclerView.ViewHolder(binding.root) {
 
+    private val imageLoader = RemoteImageLoader()
+
     fun bind(imageUrl: String) {
-        binding.imageView.loadRemoteImage(imageUrl)
+        imageLoader.loadRemoteImage(binding.imageView, imageUrl)
     }
 
     companion object {
