3603cd324fffc5431c25520fad9a7d16803a96ad
dev-cardone <dev-cardone@gmail.com>
2022-06-17 15:34:25 +0900

    feat: 이미지 크롤링 중 Progress Bar 가 표시되도록 처리

--- app/src/main/java/com/rsupport/mobile1/test/activity/ui/GettyImageListFragment.kt
+++ app/src/main/java/com/rsupport/mobile1/test/activity/ui/GettyImageListFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
@@ -43,6 +44,8 @@ class GettyImageListFragment : Fragment() {
         lifecycleScope.launchWhenResumed {
             viewModel.imageUrlBankFlow.collect(adapter::submitList)
         }
+
+        viewModel.showProgress.observe(viewLifecycleOwner) { binding.progressBar.isVisible = it }
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/rsupport/mobile1/test/activity/ui/GettyImageViewModel.kt
+++ app/src/main/java/com/rsupport/mobile1/test/activity/ui/GettyImageViewModel.kt
@@ -1,5 +1,7 @@
 package com.rsupport.mobile1.test.activity.ui
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.rsupport.mobile1.test.activity.data.GettyImageRepository
@@ -12,10 +14,17 @@ class GettyImageViewModel @Inject constructor(
     private val repository: GettyImageRepository
 ) : ViewModel() {
 
+    private val _showProgress = MutableLiveData(true)
+    val showProgress: LiveData<Boolean> = _showProgress
+
     val imageUrlBankFlow: Flow<List<String>> = repository.gettySearchResultStream
         .distinctUntilChanged()
         .shareIn(viewModelScope, SharingStarted.WhileSubscribed(5000L), 1)
-        .onStart { repository.searchImage(DEFAULT_QUERY) }
+        .onStart {
+            _showProgress.postValue(true)
+            repository.searchImage(DEFAULT_QUERY)
+            _showProgress.postValue(false)
+        }
 
     companion object {
         private const val DEFAULT_QUERY = "collaboration"
