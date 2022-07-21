72c65c9f13f12bfe5009ad3c6e1c20e7b9639191
overman623 <overman623@naver.com>
2022-07-21 13:03:12 +0900

    fix(data) : GettyImageWeb 관련 인터페이스 추가 및 클래스 구현

---
+++ app/src/main/java/com/rsupport/mobile1/test/activity/data/GettyImageWeb.kt
@@ -0,0 +1,5 @@
+package com.rsupport.mobile1.test.activity.data
+
+interface GettyImageWeb {
+    fun requestGettyImage(): Any
+}
---
+++ app/src/main/java/com/rsupport/mobile1/test/activity/data/GettyImageWebImpl.kt
@@ -0,0 +1,8 @@
+package com.rsupport.mobile1.test.activity.data
+
+class GettyImageWebImpl: GettyImageWeb {
+
+    override fun requestGettyImage(): Any {
+        return Any()
+    }
+}
