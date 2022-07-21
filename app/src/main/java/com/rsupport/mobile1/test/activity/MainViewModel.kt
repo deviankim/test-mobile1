3129d4f83ed176cf73baf0c4914b24a1876264fb
overman623 <overman623@naver.com>
2022-07-21 10:59:41 +0900

    fix(main) : 뷰모델 관련 메소드 연결 테스트 코드 수행

--- app/src/main/java/com/rsupport/mobile1/test/activity/MainViewModel.kt
+++ app/src/main/java/com/rsupport/mobile1/test/activity/MainViewModel.kt
@@ -1,5 +1,6 @@
 package com.rsupport.mobile1.test.activity
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -9,5 +10,10 @@ class MainViewModel : ViewModel() {
     private val _gettyImageList = MutableLiveData<List<Any>>()
     val gettyImageList: LiveData<List<Any>>
         get() = _gettyImageList
+
+    fun getImage() {
+        // TODO : 이미지 데이터 수신부 구현 예정
+        Log.d(MainActivity.TAG, "TEST OK")
+    }
 
 }
