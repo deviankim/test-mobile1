666b858c71d6b50c20768ec0cd521b5feed6f6c9
overman623 <overman623@naver.com>
2022-07-21 10:33:11 +0900

    fix(main) : viewModel 멤버변수 추가

--- app/src/main/java/com/rsupport/mobile1/test/activity/MainActivity.kt
+++ app/src/main/java/com/rsupport/mobile1/test/activity/MainActivity.kt
@@ -2,9 +2,14 @@ package com.rsupport.mobile1.test.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import com.rsupport.mobile1.test.R
 
 class MainActivity : AppCompatActivity() {
+
+    private val viewModel: MainViewModel by lazy {
+        ViewModelProvider(this).get(MainViewModel::class.java)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
