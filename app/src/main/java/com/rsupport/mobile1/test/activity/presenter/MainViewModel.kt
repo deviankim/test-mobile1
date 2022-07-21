be5ff463b5e3d5c713206800f45df6ce07bc58b5
overman623 <overman623@naver.com>
2022-07-21 14:05:57 +0900

    fix(viewModel) : 이미지 데이터 요청을 위한 코루틴 스코프 생성

--- app/src/main/java/com/rsupport/mobile1/test/activity/presenter/MainViewModel.kt
+++ app/src/main/java/com/rsupport/mobile1/test/activity/presenter/MainViewModel.kt
@@ -1,9 +1,13 @@
 package com.rsupport.mobile1.test.activity.presenter
 
+import android.util.Log
 import androidx.lifecycle.*
 import com.rsupport.mobile1.test.activity.domain.GettyImageRepositoryImpl
 import com.rsupport.mobile1.test.activity.domain.GettyImageResponse
+import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import kotlin.coroutines.CoroutineContext
 
 class MainViewModel(private val gettyImageRepository: GettyImageRepositoryImpl) : ViewModel(), CoroutineScope {
@@ -15,9 +19,13 @@ class MainViewModel(private val gettyImageRepository: GettyImageRepositoryImpl) 
     val gettyImageList: LiveData<List<GettyImageResponse>>
         get() = _gettyImageList
 
-    fun requestImage() {
+    fun requestImage() = launch(coroutineContext + Dispatchers.IO + CoroutineExceptionHandler { coroutineContext, throwable ->
+        Log.e(MainActivity.TAG, "Coroutine Exception : ${throwable.message}")
+        Log.e(MainActivity.TAG, "Coroutine Exception : ${throwable.stackTrace}")
+    }) {
         gettyImageRepository.getImage()
     }
+
 
     class Factory(private val gettyImageRepository: GettyImageRepositoryImpl) : ViewModelProvider.Factory {
         override fun <T : ViewModel> create(modelClass: Class<T>): T {
