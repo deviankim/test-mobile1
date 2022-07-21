af53265de3aef551d2293c1e223d292826ca38fd
overman623 <overman623@naver.com>
2022-07-21 10:57:36 +0900

    fix(viewModel) : 이미지 획득 관련 관측 데이터 추가

--- app/src/main/java/com/rsupport/mobile1/test/activity/MainViewModel.kt
+++ app/src/main/java/com/rsupport/mobile1/test/activity/MainViewModel.kt
@@ -1,8 +1,13 @@
 package com.rsupport.mobile1.test.activity
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
 
+    private val _gettyImageList = MutableLiveData<List<Any>>()
+    val gettyImageList: LiveData<List<Any>>
+        get() = _gettyImageList
 
 }
