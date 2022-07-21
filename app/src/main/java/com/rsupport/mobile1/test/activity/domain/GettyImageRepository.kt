c924b1ed320cc0faef710188d145509a55f3fecb
overman623 <overman623@naver.com>
2022-07-21 16:12:34 +0900

    fix(gettyImageRepository) : domain 패키지에서 사용하는 데이터 클래스로 파싱

--- app/src/main/java/com/rsupport/mobile1/test/activity/domain/GettyImageRepository.kt
+++ app/src/main/java/com/rsupport/mobile1/test/activity/domain/GettyImageRepository.kt
@@ -1,8 +1,5 @@
 package com.rsupport.mobile1.test.activity.domain
 
-import com.rsupport.mobile1.test.activity.data.GettyImage
-import kotlinx.coroutines.Deferred
-
 interface GettyImageRepository {
-    suspend fun getImage(): List<GettyImage>?
+    suspend fun getImage(): List<GettyImageResponse>?
 }
--- app/src/main/java/com/rsupport/mobile1/test/activity/domain/GettyImageRepositoryImpl.kt
+++ app/src/main/java/com/rsupport/mobile1/test/activity/domain/GettyImageRepositoryImpl.kt
@@ -1,11 +1,10 @@
 package com.rsupport.mobile1.test.activity.domain
 
-import com.rsupport.mobile1.test.activity.data.GettyImage
 import com.rsupport.mobile1.test.activity.data.GettyImageWebImpl
 
 class GettyImageRepositoryImpl(private val web: GettyImageWebImpl): GettyImageRepository {
 
-    override suspend fun getImage(): List<GettyImage>? {
-        return web.requestGettyImage()
+    override suspend fun getImage(): List<GettyImageResponse>? {
+        return web.requestGettyImage()?.map { it.mapper() }
     }
 }
