e11c2ab2f84f9f99e6035a21a3d682705a31178a
overman623 <overman623@naver.com>
2022-07-21 12:47:19 +0900

    fix(domin) : GettyImageResponse 클래스 추가

---
+++ app/src/main/java/com/rsupport/mobile1/test/activity/domain/GettyImageResponse.kt
@@ -0,0 +1,12 @@
+package com.rsupport.mobile1.test.activity.domain
+
+data class GettyImageResponse(
+    val author: String = "",
+    val caption: String = "",
+    val contentUrl: String = "",
+    val creditText: String = "",
+    val description: String = "",
+    val name: String = "",
+    val thumbnailUrl: String = "",
+    val uploadDate: String = ""
+)
