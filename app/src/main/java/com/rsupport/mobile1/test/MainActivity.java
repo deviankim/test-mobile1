faae7faa57a6ea7d4336af7b463b65fc7a5a6620
DESKTOP-AUPBQJO\kimco <kimcomcom0@daum.net>
2022-07-27 22:07:40 +0900

    [rsupportrnd]coding_test
    xml 생성 및 item 생성
    
    RowImgSrc 생성
    
    activity_main  ->  리사이클 뷰 추가
    item_img -> 아이템 추가를 위해 생성
    item_loading -> 유저 가독성을 위한 progressbar 추가

---
+++ app/src/main/java/com/rsupport/mobile1/test/MainActivity.java
@@ -0,0 +1,6 @@
+package com.rsupport.mobile1.test;
+
+import android.app.Activity;
+
+public class MainActivity extends Activity {
+}
---
+++ app/src/main/java/com/rsupport/mobile1/test/item/RowImgSrc.kt
@@ -0,0 +1,7 @@
+package com.rsupport.mobile1.test.item
+
+data class RowImgSrc(
+    val leftImgSrc: String,
+    val middleImgSrc: String,
+    val rightImgSrc: String
+)
