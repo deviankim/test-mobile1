02c28525a19b3f9b429fc69a25909696e8961545
lee <lsh3293@gmail.com>
2022-08-23 21:11:46 +0900

    1. Utility 클래스 추가 - LogUtil.kt

--- app/src/main/java/com/rsupport/mobile1/test/activity/MainActivity.kt
+++ app/src/main/java/com/rsupport/mobile1/test/activity/MainActivity.kt
@@ -3,10 +3,12 @@ package com.rsupport.mobile1.test.activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.rsupport.mobile1.test.R
+import com.rsupport.mobile1.test.activity.util.LogUtil
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        LogUtil.d("")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
---
+++ app/src/main/java/com/rsupport/mobile1/test/activity/util/LogUtil.kt
@@ -0,0 +1,35 @@
+package com.rsupport.mobile1.test.activity.util
+
+import android.util.Log
+
+object LogUtil {
+
+    private const val LOG_TAG = "Mobile1TestApp"
+    private const val EXT_KOTLIN = ".kt"
+    private const val BLANK = ""
+    private const val OPEN = "["
+    private const val CLOSE = "]"
+    private const val SEPARATOR_1 = "::"
+    private const val SEPARATOR_2 = "()"
+    private const val SEPARATOR_3 = " >> "
+
+    fun d(message: String) {
+        Log.d(LOG_TAG, message.toLog())
+    }
+
+    private fun String.toLog(): String {
+        val ste = Thread.currentThread().stackTrace[4]
+        return StringBuilder()
+            .append(ste.fileName.replace(EXT_KOTLIN, BLANK))
+            .append(SEPARATOR_1)
+            .append(ste.methodName)
+            .append(SEPARATOR_2)
+            .append(OPEN)
+            .append(ste.lineNumber)
+            .append(CLOSE)
+            .append(SEPARATOR_3)
+            .append(this)
+            .toString()
+    }
+
+}
