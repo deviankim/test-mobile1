c0612050543b352466adf64fc20952c04c8b3106
lee <lsh3293@gmail.com>
2022-08-24 21:21:42 +0900

    1. Glide 라이브러리 추가

---
+++ app/src/main/java/com/rsupport/mobile1/test/GlideModule.kt
@@ -0,0 +1,7 @@
+package com.rsupport.mobile1.test
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+@GlideModule
+class GlideModule : AppGlideModule()
