1e3c6ef2ab5f36c9e8ff98166ad3e5fe55344f31
gurumdevv <gurumdevv@gmail.com>
2024-05-01 17:58:36 +0900

    feat: 웹 사이트로부터 데이터를 받아오는 WebScrapper 클래스 작성

---
+++ app/src/main/java/com/rsupport/mobile1/test/activity/data/network/WebScrapper.kt
@@ -0,0 +1,41 @@
+package com.rsupport.mobile1.test.activity.data.network
+
+import com.rsupport.mobile1.test.activity.data.model.ImageInformation
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import org.jsoup.HttpStatusException
+import org.jsoup.Jsoup
+import org.jsoup.select.Elements
+
+private const val BASE_URL =
+    "https://www.gettyimages.com/photos/collaboration?assettype=image&page="
+private const val URL_SUFFIX = "&phrase=collaboration&sort=mostpopular&license=rf,rm"
+private const val GALLERY_MOSAIC_ASSET_SELECTOR = "div[data-testid=galleryMosaicAsset]"
+private const val MOSAIC_ASSET_TITLE_SELECTOR = "a[data-testid=mosaicAssetTitle]"
+
+class WebScrapper {
+
+    suspend fun getImageInformation(pageId: Int): Result<List<ImageInformation>> =
+        withContext(Dispatchers.IO) {
+            val itemList = mutableListOf<ImageInformation>()
+
+            try {
+                val doc =
+                    Jsoup.connect("$BASE_URL$pageId$URL_SUFFIX")
+                        .get()
+                val elements: Elements = doc.select(GALLERY_MOSAIC_ASSET_SELECTOR)
+
+                for (element in elements) {
+                    val title = element.select(MOSAIC_ASSET_TITLE_SELECTOR).text()
+                    val imageUrl = element.select("img").attr("src")
+                    itemList.add(ImageInformation(title, imageUrl))
+                }
+                Result.success(itemList.toList())
+            } catch (e: HttpStatusException) {
+                //잘못된 page id를 요청하는 경우 HTTP 404 오류 발생 == 더 이상 자료 없음
+                Result.success(emptyList())
+            } catch (e: Exception) {
+                Result.failure(e)
+            }
+        }
+}
