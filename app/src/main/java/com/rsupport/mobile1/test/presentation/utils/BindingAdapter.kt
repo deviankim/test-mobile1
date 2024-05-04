f53e9b1309708d51c8482d20e14c6aa9b43c1060
dev-baik <wlsdn9297@naver.com>
2024-05-04 15:17:51 +0900

    feat: ImageList image binding adapter 추가

---
+++ app/src/main/java/com/rsupport/mobile1/test/presentation/utils/BindingAdapter.kt
@@ -0,0 +1,16 @@
+package com.rsupport.mobile1.test.presentation.utils
+
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+
+object BindingAdapter {
+
+    @JvmStatic
+    @BindingAdapter("bindImage")
+    fun setImage(imageView: ImageView, imageUrl: String) {
+        Glide.with(imageView.context)
+            .load(imageUrl)
+            .into(imageView)
+    }
+}
