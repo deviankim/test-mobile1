d19a03467646406b06c25c09b7696c8daf295421
dev-baik <wlsdn9297@naver.com>
2024-05-07 17:34:59 +0900

    feat: rvMainList에서 ViewType에 따라 컬럼 변경 추가

--- app/src/main/java/com/rsupport/mobile1/test/presentation/main/ui/MainActivity.kt
+++ app/src/main/java/com/rsupport/mobile1/test/presentation/main/ui/MainActivity.kt
@@ -7,9 +7,15 @@ import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.flowWithLifecycle
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.GridLayoutManager
 import com.rsupport.mobile1.test.R
 import com.rsupport.mobile1.test.databinding.ActivityMainBinding
-import com.rsupport.mobile1.test.presentation.main.ui.list.MainListAdapter
+import com.rsupport.mobile1.test.presentation.main.ui.list.MainRecyclerViewAdapter
+import com.rsupport.mobile1.test.presentation.main.ui.list.MainRecyclerViewAdapter.Companion.VIEW_TYPE_BOTTOM
+import com.rsupport.mobile1.test.presentation.main.ui.list.MainRecyclerViewAdapter.Companion.VIEW_TYPE_LIST
+import com.rsupport.mobile1.test.presentation.main.viewmodel.MainViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -17,12 +23,12 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private val viewModel: MainViewModel by viewModels()
 
-    private lateinit var mainListAdapter: MainListAdapter
+    private lateinit var mainRecyclerViewAdapter: MainRecyclerViewAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        initTodoListRecyclerView()
+        initMainRecyclerView()
         observeMainUiState()
     }
 
@@ -48,8 +54,27 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun initTodoListRecyclerView() {
-        mainListAdapter = MainListAdapter()
-        binding.rvMainList.adapter = mainListAdapter
+    private fun initMainRecyclerView() {
+        mainRecyclerViewAdapter = MainRecyclerViewAdapter()
+        binding.rvMainList.adapter = mainRecyclerViewAdapter
+        binding.rvMainList.layoutManager = createGridLayoutManager()
+    }
+
+    private fun createGridLayoutManager(): GridLayoutManager {
+        return GridLayoutManager(this, 3).apply {
+            spanSizeLookup = createSpanSizeLookup(spanCount)
+        }
+    }
+
+    private fun createSpanSizeLookup(spanCount: Int): GridLayoutManager.SpanSizeLookup {
+        return object : GridLayoutManager.SpanSizeLookup() {
+            override fun getSpanSize(position: Int): Int {
+                return when (mainRecyclerViewAdapter.getItemViewType(position)) {
+                    VIEW_TYPE_LIST -> 1
+                    VIEW_TYPE_BOTTOM -> spanCount
+                    else -> throw IllegalArgumentException("ViewType is not valid")
+                }
+            }
+        }
     }
 }
