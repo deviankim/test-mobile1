9b298e4744c59992530d39f0beba1516a86d34cd
dev-baik <wlsdn9297@naver.com>
2024-05-08 19:29:59 +0900

    refactor: MainResponse to MainList 함수 추가

--- app/src/main/java/com/rsupport/mobile1/test/data/source/mapper/MainMapper.kt
+++ app/src/main/java/com/rsupport/mobile1/test/data/source/mapper/MainMapper.kt
@@ -3,29 +3,27 @@ package com.rsupport.mobile1.test.data.source.mapper
 import com.rsupport.mobile1.test.data.model.main.MainDTO
 import com.rsupport.mobile1.test.data.model.main.MainResponse
 import com.rsupport.mobile1.test.domain.model.HtmlParseResult
-import com.rsupport.mobile1.test.domain.model.MainRecyclerViewItem
 import com.rsupport.mobile1.test.domain.model.MainList
+import com.rsupport.mobile1.test.domain.model.MainRecyclerViewItem
 
 object MainMapper {
 
     fun HtmlParseResult<MainResponse>.toMainDomain(): HtmlParseResult<MainList> {
         return when (this) {
-            is HtmlParseResult.Success -> {
-                val mainItems = data.contents.map { it.toItem() }
-                val combinedList = mainItems + MainRecyclerViewItem.PageNumber(pageNumber = 1)
-                HtmlParseResult.Success(MainList(combinedList))
-            }
-
-            is HtmlParseResult.Error -> {
-                HtmlParseResult.Error(exception)
-            }
+            is HtmlParseResult.Success -> HtmlParseResult.Success(data.toMainList())
+            is HtmlParseResult.Error -> HtmlParseResult.Error(exception)
         }
     }
 
-    private fun MainDTO.toItem(): MainRecyclerViewItem.MainItem {
-        return MainRecyclerViewItem.MainItem(
+    private fun MainResponse.toMainList(): MainList {
+        val mainItems = contents.map { it.toItem() }
+        val combinedList = mainItems + MainRecyclerViewItem.PageNumber(pageNumber = 1)
+        return MainList(combinedList)
+    }
+
+    private fun MainDTO.toItem(): MainRecyclerViewItem.MainItem =
+        MainRecyclerViewItem.MainItem(
             url = this.url,
             title = this.title
         )
-    }
 }
