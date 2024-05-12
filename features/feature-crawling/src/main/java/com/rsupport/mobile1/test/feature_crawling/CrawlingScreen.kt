971777f4044e2cdd2539a63ca0c4e105b179fbd2
임성우 <seongwoorang@naver.com>
2024-05-12 16:56:18 +0900

    [Chore] - Add Libraries Modules

--- features/feature-crawling/src/main/java/com/rsupport/mobile1/test/feature_crawling/CrawlingScreen.kt
+++ features/feature-crawling/src/main/java/com/rsupport/mobile1/test/feature_crawling/CrawlingScreen.kt
@@ -12,3 +12,15 @@ fun CrawlingScreen(navHostController: NavHostController) {
     }
 
 }
+
+/*
+override fun getImageUrlList(page: Int): Flow<List<String>> = flow {
+    val url = "https://www.gettyimages.com/photos/collaboration?assettype=image&sort=mostpopular&phrase=collaboration&license=rf,rm&page=$page"
+    val doc = Jsoup.connect(url).get()
+    val imageUrls = doc
+        .select("picture")
+        .select("img[src]")
+        .map { element -> element.attr("src") }
+    emit(imageUrls)
+}.flowOn(Dispatchers.IO)
+ */
