c2d773c7cebe67e996cd827b79bd8a04866a7ce1
Jin-sfn <wjin.kim@smartfoodnet.com>
2024-05-22 17:55:19 +0900

    [김위진] domain entity 클래스 주석 추가

--- domain/src/main/java/com/kimwijin/domain/model/ImageInfo.kt
+++ domain/src/main/java/com/kimwijin/domain/model/ImageInfo.kt
@@ -1,5 +1,12 @@
 package com.kimwijin.domain.model
 
+/**
+ * 이미지 정보를 가지는 도메인 모델 클래스 입니다.
+ * 이 클래스는 비즈니스 요구사항을 구현하고, 다양한 서비스 계층과 데이터 계층 사이에서 정보를 전달하는 역할을 합니다.
+ *
+ * @author (김위진)
+ * @since (2024-05-22)
+ */
 data class ImageInfo(
     val src: String,
     val title: String,
