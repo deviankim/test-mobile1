1e760e110cadc2a5169de0d7567bef79d1971b74
yongsubs1 <yongsubs1@gmail.com>
2024-07-28 18:15:55 +0900

    [Test-1] make project for Rsupport coding Test 1. https://www.gettyimages.com/photos/collaboration 의 내용을 리스트 형태로 출력합니다. 2. 컬럼은 3개입니다. 3. 이미지를 가져오는데 있어, Getty api 를 사용하는게 아닌, 다른 방식으로의 처리를 부탁드립니다.(오픈소스 사용가능합니다.) 4. (6년차 이상만 해당) 이미지 데이터를 받은 후, 내부적으로 처리하는 부분에 있어서는 오픈소스의 사용은 가급적 금해주시고, 개발언어의 기본 api 만을 이용하여 구현해주시길 부탁드립니다.

--- app/src/main/java/com/rsupport/mobile1/test/activity/ImageAdapter.kt
+++ app/src/main/java/com/rsupport/mobile1/test/activity/ImageAdapter.kt
@@ -1,13 +1,17 @@
 package com.rsupport.mobile1.test.activity
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.os.AsyncTask
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 import android.widget.ImageView
-import com.bumptech.glide.Glide
 import com.rsupport.mobile1.test.R
+import java.net.HttpURLConnection
+import java.net.URL
 
 class ImageAdapter(private val context: Context, private val images: List<String>) : BaseAdapter() {
 
@@ -21,8 +25,33 @@ class ImageAdapter(private val context: Context, private val images: List<String
         val view: View = convertView ?: LayoutInflater.from(context).inflate(R.layout.image_item, parent, false)
         val imageView: ImageView = view.findViewById(R.id.imageView)
 
-        Glide.with(context).load(images[position]).into(imageView)
+        val imageUrl = images[position]
+        LoadImage(imageView).execute(imageUrl)
 
         return view
+    }
+
+    private class LoadImage(val imageView: ImageView) : AsyncTask<String, Void, Bitmap?>() {
+
+        override fun doInBackground(vararg params: String?): Bitmap? {
+            val urlString = params[0]
+            return try {
+                val url = URL(urlString)
+                val connection = url.openConnection() as HttpURLConnection
+                connection.doInput = true
+                connection.connect()
+                val inputStream = connection.inputStream
+                BitmapFactory.decodeStream(inputStream)
+            } catch (e: Exception) {
+                e.printStackTrace()
+                null
+            }
+        }
+
+        override fun onPostExecute(result: Bitmap?) {
+            if (result != null) {
+                imageView.setImageBitmap(result)
+            }
+        }
     }
 }
