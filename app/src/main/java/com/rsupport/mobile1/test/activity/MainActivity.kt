dd666bb5a34f609f1a5936ed55e46c79dd930e23
kamamerona <zszs2020@naver.com>
2024-08-10 17:03:06 +0900

    [feat] binding 추가

--- app/src/main/java/com/rsupport/mobile1/test/activity/MainActivity.kt
+++ app/src/main/java/com/rsupport/mobile1/test/activity/MainActivity.kt
@@ -3,11 +3,19 @@ package com.rsupport.mobile1.test.activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.rsupport.mobile1.test.R
+import com.rsupport.mobile1.test.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        init()
+    }
+
+    private fun init() {
+        
     }
 }
