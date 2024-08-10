89286c124209417e112ec4ee608115ac6ec8c5e3
kamamerona <zszs2020@naver.com>
2024-08-10 17:18:39 +0900

    [feat] image adapter 연결 및 button 클릭 리스너 추가

--- app/src/main/java/com/rsupport/mobile1/test/activity/MainActivity.kt
+++ app/src/main/java/com/rsupport/mobile1/test/activity/MainActivity.kt
@@ -2,11 +2,13 @@ package com.rsupport.mobile1.test.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.rsupport.mobile1.test.R
+import androidx.recyclerview.widget.GridLayoutManager
+import com.rsupport.mobile1.test.activity.adapter.ImageAdapter
 import com.rsupport.mobile1.test.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private val adapter = ImageAdapter(mutableListOf())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,6 +18,14 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun init() {
-        
+        val gridLayoutManager = GridLayoutManager(this, 3)
+        binding.recyclerImageList.layoutManager = gridLayoutManager
+        binding.recyclerImageList.adapter = adapter
+
+        onButtonClick() // 버튼 클릭 리스너
+    }
+
+    private fun onButtonClick() {
+        binding.buttonSearch.setOnClickListener {}
     }
 }
