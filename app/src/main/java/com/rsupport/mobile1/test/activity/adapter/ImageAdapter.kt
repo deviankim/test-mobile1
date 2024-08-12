db3ff30d43ff675afcf53b42ae8c9a61503233b6
KamaTAEWOO <zszs2020@naver.com>
2024-08-12 21:04:07 +0900

    [feat] 이미지 로드 시 기본 api 사용

--- app/src/main/java/com/rsupport/mobile1/test/activity/adapter/ImageAdapter.kt
+++ app/src/main/java/com/rsupport/mobile1/test/activity/adapter/ImageAdapter.kt
@@ -1,10 +1,15 @@
 package com.rsupport.mobile1.test.activity.adapter
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.os.AsyncTask
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
 import com.rsupport.mobile1.test.databinding.ItemImageBinding
+import java.net.HttpURLConnection
+import java.net.URL
 
 class ImageAdapter(private var imgUrlData: List<String>) : RecyclerView.Adapter<ImageAdapter.ImageViewHolder>() {
 
@@ -17,9 +22,13 @@ class ImageAdapter(private var imgUrlData: List<String>) : RecyclerView.Adapter<
 
     override fun onBindViewHolder(holder: ImageViewHolder, position: Int) {
         val imgUrl = imgUrlData[position]
-        Glide.with(holder.binding.imageRowItem.context)
+        loadImage(holder.binding.imageRowItem, imgUrl)
+
+        /*
+            Glide.with(holder.binding.imageRowItem.context)
             .load(imgUrl)
             .into(holder.binding.imageRowItem)
+        */
     }
 
     override fun getItemCount() = imgUrlData.size
@@ -27,5 +36,33 @@ class ImageAdapter(private var imgUrlData: List<String>) : RecyclerView.Adapter<
     fun updateData(newData: List<String>) {
         imgUrlData = newData
         notifyDataSetChanged()
+    }
+
+    private fun loadImage(imageView: ImageView, imageUrl: String) {
+        ImageLoadTask(imageView).execute(imageUrl)
+    }
+
+    private class ImageLoadTask(private val imageView: ImageView) : AsyncTask<String, Void, Bitmap?>() {
+
+        override fun doInBackground(vararg params: String): Bitmap? {
+            val imageUrl = params[0]
+            return try {
+                val url = URL(imageUrl)
+                val connection: HttpURLConnection = url.openConnection() as HttpURLConnection
+                connection.doInput = true
+                connection.connect()
+                val inputStream = connection.inputStream
+                BitmapFactory.decodeStream(inputStream)
+            } catch (e: Exception) {
+                e.printStackTrace()
+                null
+            }
+        }
+
+        override fun onPostExecute(result: Bitmap?) {
+            if (result != null) {
+                imageView.setImageBitmap(result)
+            }
+        }
     }
 }
