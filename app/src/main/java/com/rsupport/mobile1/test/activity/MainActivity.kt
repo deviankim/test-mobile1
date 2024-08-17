33b8767fcdbee6c3fcbbfe743a6853e378c76a85
changhyun <cg072@naver.com>
2024-08-17 12:12:19 +0900

    Feat - ViewBinding 추가 및 적용
    1. build.gradle에 ViewBinding 사용설정
    2. MainActivity에 ViewBinding 사용

--- app/src/main/java/com/rsupport/mobile1/test/activity/MainActivity.kt
+++ app/src/main/java/com/rsupport/mobile1/test/activity/MainActivity.kt
@@ -3,13 +3,18 @@ package com.rsupport.mobile1.test.activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.rsupport.mobile1.test.R
+import com.rsupport.mobile1.test.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
