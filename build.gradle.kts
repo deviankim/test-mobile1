23b9dc6a2b329adefe917495ee06df6a8c4c6061
seunghwan <seosh817@naver.com>
2024-08-20 13:16:51 +0900

    build: Gradle 최신화

---
+++ build.gradle.kts
@@ -0,0 +1,11 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.4.2")
+    }
+}
+
+plugins {
+    id("com.android.application") version "8.1.4" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
+}
